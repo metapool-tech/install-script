6bb3237d2bf3cc493012496f880214444515e34a
sven <s@ono.re>
2022-01-19 21:38:57 +0100

    update: bzminer install

--- linux/metapool-mining.sh
+++ linux/metapool-mining.sh
@@ -2,13 +2,12 @@
 # Made by opimon, svenhash
 set -e
 
-PROXY_VERSION=1.0.1
-MINER_VERSION=0.5.4
-MINER_VERSION_AMD=0.2.0
-SCRIPT_VERSION=1.2.0
+BZMINER_VERSION=7.1.5
+SCRIPT_VERSION=1.3.0
 
 DIR=$(pwd)
 PKG_MANAGER=""
+BZMINER_FOLDER=$DIR/bzminerv${BZMINER_VERSION}
 
 GREEN="\e[0;92m"
 YELLOW="\e[0;93m"
@@ -18,7 +17,7 @@ echo -e ""
 echo -e "script version: ${SCRIPT_VERSION}"
 
 
-if ! command -v wget &> /dev/null; then
+if ! command -v curl &> /dev/null; then
 
    declare -A OS_INFO;
    OS_INFO[/etc/redhat-release]=yum
@@ -36,44 +35,45 @@ if ! command -v wget &> /dev/null; then
    done
 
    if [ -z $PKG_MANAGER ]; then
-     echo "OS cannot be detected. Install wget manually" 
+     echo "OS cannot be detected. Install curl manually" 
   else
-     ${PKG_MANAGER} update && ${PKG_MANAGER} -y install wget
+     ${PKG_MANAGER} update && ${PKG_MANAGER} -y install curl
   fi
 
 fi
 
-
-if [[ $( cut -f1,2,18 /proc/bus/pci/devices | grep -c nvidia ) -gt 0 ]]; then
-   echo -e "Downloading nvidia miner v${MINER_VERSION}" 
-   wget -q https://github.com/alephium/gpu-miner/releases/download/v${MINER_VERSION}/alephium-${MINER_VERSION}-cuda-miner-linux -O alephium-miner-linux 
-   wget -q https://github.com/alephium/gpu-miner/releases/download/v0.4.4/alephium-0.4.4-cuda-miner-linux -O alephium-cuda-miner-linux-workaround
-else
-   echo -e "Downloading amd miner v${MINER_VERSION_AMD}" 
-   wget -q  https://github.com/alephium/amd-miner/releases/download/v0.2.0/alephium-0.2.0-amd-miner-linux -O alephium-miner-linux
-fi
-
-echo -e "Downloading mining-proxy v${PROXY_VERSION}"
-wget -q https://github.com/alephium/mining-proxy/releases/download/v${PROXY_VERSION}/alephium-mining-proxy-${PROXY_VERSION}-linux -O alephium-mining-proxy-linux
+mkdir ${BZMINER_FOLDER}
+wget https://www.bzminer.com/downloads/bzminer_v${BZMINER_VERSION}_linux.tar.gz | tar -xz -C ${BZMINER_FOLDER}
 
 echo -e ""
 
-if [[ ! -f config.json ]] || [[ "$1" == "-r" ]]; then
+if [[ ! -f ${BZMINER_FOLDER}/config.txt ]] || [[ "$1" == "-r" ]]; then
 
-cat <<EOT > config.json
+cat <<EOT > ${BZMINER_FOLDER}/config.txt
 {
-    "logPath": "./logs/",
-    "diff1TargetNumZero": 30,
-    "serverHost": "eu.metapool.tech",
-    "serverPort": 20032,
-    "proxyPort": 30032,
-    "workerName": "",
-    "address": "your-mining-address"
+    "pool_configs": [{
+            "algorithm": "alph",
+            "wallet": ["your-mining-address"], // replace 0000 with your own wallet addresses
+            "url": ["stratum+tcp://eu.metapool.tech:20032"], // replace with the pool you wish to mine to. Add multiple pools for failsafe when a pool fails
+            "username": "worker_name",
+            "lhr_only": false
+        }],
+    "pool": [0], // pool config you wish to use by default when device_overrides does not specify
+    "rig_name": "rig",
+    "log_file": "",
+    "nvidia_only": false,
+    "amd_only": false,
+    "auto_detect_lhr": true,
+    "lock_config": false,
+    "advanced_config": false,
+    "advanced_display_config": false,
+    "device_overrides": []
 }
+
 EOT
 fi
 
-cat << EOT > metapool-run.sh
+cat << EOT > ${BZMINER_FOLDER}/metapool-run.sh
 #!/usr/bin/env bash
 set -e
 
@@ -82,45 +82,34 @@ RED="\e[0;91m"
 GREEN="\e[0;92m"
 RESET="\e[0m"
 
-if [[ ! -f config.json ]]
+if [[ ! -f config.txt ]]
 then
-   echo -e "\${RED}Error: \$DIR/config.json not found\${RESET}"
+   echo -e "\${RED}Error: \$DIR/config.txt not found\${RESET}"
    exit 1
 fi
 
 if [[ \$1 == "-a" ]] && [ ! -z \$2 ]; then
    ADDR="\$2"
-   echo -e "\${GREEN}New address: \$2\${RESET}"
-   sed -i 's/\"address\".*/\"address\": \"'\${ADDR}'\"/ig' config.json
-
-   if grep -q -wi ".*addresses.*" \$DIR/config.json; then   
-      NEW_CONFIG=\$(jq '(del(.addresses))' config.json) 
-      NEW_CONFIG=\$(jq '.+ {"address": "'\${ADDR}'"}' <<<\$NEW_CONFIG)
-      echo \$NEW_CONFIG|jq > \$DIR/config.json 
-   fi
-
-
+   echo -e "\${GREEN}Address: \$2\${RESET}"
+   sed -i 's/\"wallet\".*/\"wallet\": [\"'\${ADDR}'\"]/ig' config.txt
 fi
 
-if grep -q -wi ".*your-mining.*" \$DIR/config.json; then
+if grep -q -wi ".*your-mining.*" \$DIR/config.txt; then
    echo -e "\${RED}Error: Address is not set"
-   echo -e "\${RED}Set your mining addresses in \$DIR/config.json \${RESET}"
+   echo -e "\${RED}Set your address with ./metapool-run.sh -a <your address> or in \$DIR/config.txt\${RESET}"
    exit 1
 fi
 
 
-trap "trap - SIGTERM && kill -- -\$$" SIGINT SIGTERM EXIT
-./alephium-mining-proxy-linux config.json &
-
-./alephium-miner-linux  -p 30032 || ./alephium-cuda-miner-linux-workaround -p 30032 
 
 wait
 
 EOT
 
-chmod +x alephium* metapool-run.sh
+cd $BZMINER_FOLDER
+chmod +x  metapool-run.sh
 
-if grep -q -wi ".*your-mining.*" $DIR/config.json; then
+if grep -q -wi ".*your-mining.*" config.txt; then
    echo -e "${YELLOW}Set your address with ./metapool-run.sh -a <your address>${RESET}"
    echo -e ""
 fi
